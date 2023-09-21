f6369d716665a7b6161532beab7c5c6c4ed9d2d5
cestari15 <rafacjacobs1502@gmail.com>
2023-09-21 15:17:51 -0300

    Commit geral

--- src/components/BuscaCepForm.tsx
+++ src/components/BuscaCepForm.tsx
@@ -1,6 +1,8 @@
 import React, {useState, ChangeEvent, FormEvent, useEffect} from "react";
 
-import styles from "..App.module.css";
+import styles from "../App.module.css";
+import Footer from "./Footer";
+import Header from "./Header";
 const BuscaCep = () => {
 
     const [cep, setCep] = useState<string>("");
@@ -38,6 +40,8 @@ const BuscaCep = () => {
 
     return (
         <div>
+            <Header/>
+            <main className={styles.main}>
             <form onSubmit={findCep}>
                 <label htmlFor="cep">CEP</label>
                 <input type="text" name="cep" id="cep" onChange={submitForm}/>
@@ -47,7 +51,10 @@ const BuscaCep = () => {
             <p>Estado: {uf}</p> 
             <p>CEP: {cep}</p> 
             <p className={styles.error}>{erro}</p>
+            </main>
+        <Footer/>
         </div>
+        
     );
 }
 
